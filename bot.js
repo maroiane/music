5ab81c1c1e717b9901cc1ca04a4a7829b5213a52
maroiane <40382446+maroiane@users.noreply.github.com>
2019-05-05 20:32:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,6 @@ const client = new Discord.Client();
 const PREFIX = "~"
 const Util = require('discord.js');
  
-const getYoutubeID = require('get-youtube-id');
- 
-const fetchVideoInfo = require('youtube-info');
- 
-const YouTube = require('simple-youtube-api');
- 
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
- 
-const queue = new Map();
- 
-const ytdl = require('ytdl-core');
- 
-const fs = require('fs');
- 
-const gif = require("gif-search");
- 
  
  
 console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
